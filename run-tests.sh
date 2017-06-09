7be44c008e228200d46a5b6003bdf8535e211a1a
Paweł Albecki <palbecki@soldevelo.com>
2017-06-09 17:06:45 +0200

    OLMIS-2236: get rid of groovy, improve output

--- run-tests.sh
+++ run-tests.sh
@@ -10,19 +10,41 @@ clean() {
     /usr/local/bin/docker-compose -f docker-compose.stable-version.yml down --volumes > /dev/null 2> /dev/null
 }
 
-checkContainers() {
-    groovy ../wait_for_containers.groovy $1 $2
-    if [[ $? -eq 0 ]]; then
-        break
-    fi
-
-    if [[ $3 -eq 50 ]]; then
-        echo "TIMED OUT WAITING FOR CONTAINER"
-        clean
-        exit 1
-    fi
-
+wait_for_services() {
+    # we need to give fixed time for services registration
     sleep 5
+    services_list=`curl -s 'http://localhost:8500/v1/catalog/services' | sed -e 's/[{}"]/''/g' | awk -v RS=',' -F: '{print $1}' |  grep -v consul | grep -v reference-ui | paste -sd ","`
+    IFS=',' read -r -a services_array <<< "$services_list"
+
+    echo "waiting for ${services_array[*]} to be started up and serving"
+    for service in "${services_array[@]}"
+    do
+        counter=0
+        while [[ $counter -lt 50 ]]; do
+            let counter=counter+1
+            echo "trying $service $counter times"
+            service_response=`curl ${BASE_URL}/$service 2> /dev/null`
+            if [[ $service_response == {* ]]; then
+                break
+            fi
+            if [[ $counter == 50 ]]; then
+                echo "TIMED OUT WAITING FOR SERVICE $service"
+                clean
+                exit 1
+            fi
+            if [[ $1 == "test" ]]; then
+                test_result=`/usr/local/bin/docker-compose -f docker-compose.new-version.yml exec log sh -c "cat /var/log/messages" | grep ERROR | wc -l`
+                if [[ $test_result -ne 0 ]]; then
+                    echo '============ LOG ERRORS FROM STARTING NEW CONTAINERS ============'
+                    /usr/local/bin/docker-compose -f docker-compose.new-version.yml exec log sh -c "cat /var/log/messages"
+                    echo "MIGRATION TESTS FAILURE"
+                    #clean
+                    exit 1
+                fi
+            fi
+            sleep 5
+        done
+    done
 }
 
 mkdir -p build
@@ -34,9 +56,7 @@ curl https://raw.githubusercontent.com/OpenLMIS/openlmis-ref-distro/${STABLE_VER
 echo 'DOWNLOADING DOCKER COMPOSE FOR NEW VERSION'
 curl https://raw.githubusercontent.com/OpenLMIS/openlmis-ref-distro/${NEW_VERSION:-master}/docker-compose.yml > docker-compose.new-version.yml
 
-/usr/local/bin/docker-compose -f docker-compose.stable-version.yml down --volumes
-/usr/local/bin/docker-compose -f docker-compose.new-version.yml down --volumes
-
+clean
 /usr/local/bin/docker-compose -f docker-compose.stable-version.yml pull
 
 echo 'STARTING OLD COMPONENT VERSIONS THAT WILL LOAD OLD DEMO DATA TO DATABASE'
@@ -46,42 +66,17 @@ set -o allexport
 source .env
 set +o allexport
 
-# we need to give fixed time for services registration
-sleep 5
-services_list=`curl -s 'http://localhost:8500/v1/catalog/services' | sed -e 's/[{}"]/''/g' | awk -v RS=',' -F: '{print $1}' |  grep -v consul | grep -v reference-ui | paste -sd ","`
-
-counter=0
-while [[ $counter -lt 50 ]]; do
-    let counter=counter+1
-    checkContainers ${BASE_URL} $services_list $counter
-done
-
-/usr/local/bin/docker-compose -f docker-compose.stable-version.yml stop
-
-docker rm -f `/usr/local/bin/docker-compose -f docker-compose.stable-version.yml ps | awk '{ print $1 }' | grep build | grep -v db | paste -sd " "`
+wait_for_services
+echo 'REMOVING OLD CONTAINERS EXCEPT DATABASE'
+/usr/local/bin/docker-compose -f docker-compose.stable-version.yml stop > /dev/null 2> /dev/null
+docker rm -f `/usr/local/bin/docker-compose -f docker-compose.stable-version.yml ps | awk '{ print $1 }' | grep build | grep -v db | paste -sd " "` > /dev/null 2> /dev/null
 
 echo 'STARTING NEW COMPONENT VERSIONS WITH PRODUCTION FLAG (NO DATA LOSS)'
 export spring_profiles_active=production
 /usr/local/bin/docker-compose -f docker-compose.new-version.yml up -d
 
-counter=0
-test_result=1
-while [[ $counter -lt 50 ]]; do
-    test_result=`/usr/local/bin/docker-compose -f docker-compose.new-version.yml exec log sh -c "cat /var/log/messages" | grep ERROR | wc -l`
-    if [[ $test_result -ne 0 ]]; then
-        break
-    fi
-    let counter=counter+1
-    checkContainers ${BASE_URL} $services_list $counter
-done
+wait_for_services test
 
-if [[ $test_result -ne 0 ]]; then
-    echo '============ LOG ERRORS FROM STARTING NEW CONTAINERS ============'
-    /usr/local/bin/docker-compose -f docker-compose.new-version.yml exec log sh -c "cat /var/log/messages"
-else
-    echo "MIGRATION TESTS SUCCESS"
-fi
-
+echo "MIGRATION TESTS SUCCESS"
 clean
-
 exit ${test_result}
